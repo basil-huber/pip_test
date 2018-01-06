fe0f6a6a855767648ab115aa8f028cc97f19d1c9
Basil Huber <basil.huber@gmail.com>
2018-01-06 12:32:01 +0100

    Renamed to piptest

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pip_test',
+setup(name='piptest',
       version='0.1',
       description='The funniest joke in the world',
       url='git@github.com:basil-huber/pip_test.git',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
-      packages=['pip_test'],
+      packages=['piptest'],
       zip_safe=False)
